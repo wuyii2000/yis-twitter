a70e23d7784088ccd4b1fa9af18d1ac8affefd19
Yi Wu <yiwu@Yis-MacBook-Air.local>
2022-05-18 17:01:40 -0700

    Create api and test for comments

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -15,6 +15,10 @@ class UserSerializerForTweet(serializers.ModelSerializer):
         fields = ('id', 'username')
 
 
+class UserSerializerForComment(UserSerializerForTweet):
+    pass
+
+
 class UserSerializerForFriendship(UserSerializerForTweet):
     pass
 
