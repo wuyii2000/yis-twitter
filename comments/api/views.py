e669b9b83423dff1f39b23de0c3f11d654e5c694
Yi Wu <yiwu@Yis-MacBook-Air.local>
2022-05-25 14:27:37 -0700

    Install notifications

--- comments/api/views.py
+++ comments/api/views.py
@@ -9,6 +9,7 @@ from comments.api.serializers import (
 )
 from comments.api.permissions import IsObjectOwner
 from utils.decorators import required_params
+from inbox.services import NotificationService
 
 
 class CommentViewSet(viewsets.GenericViewSet):
@@ -47,6 +48,7 @@ class CommentViewSet(viewsets.GenericViewSet):
             }, status=status.HTTP_400_BAD_REQUEST)
 
         comment = serializer.save()
+        NotificationService.send_comment_notification(comment)
         return Response(
             CommentSerializer(comment, context={'request': request}).data,
             status=status.HTTP_201_CREATED
