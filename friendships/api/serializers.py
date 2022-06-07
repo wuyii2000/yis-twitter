518378688289f8b10f8346de255e164117b38d5a
Yi Wu <yiwu@Yis-MacBook-Air.local>
2022-06-07 15:48:35 -0700

    friendship pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -3,22 +3,37 @@ from friendships.models import Friendship
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import User
+from friendships.services import FriendshipService
 
 
 class FollowerSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='from_user')
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        # TODO This part has SQL operation on every obj, needs to be improved
+        return FriendshipService.has_followed(self.context['request'].user, obj.from_user)
 
 
 class FollowingSerializer(serializers.ModelSerializer):
     user = UserSerializerForFriendship(source='to_user')
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        # TODO This part has SQL operation on every obj, needs to be improved
+        return FriendshipService.has_followed(self.context['request'].user, obj.to_user)
 
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
