6881c63d3a6e43ba508788ce373c83e14d5f1dc5
Yi Wu <yiwu@Yis-MacBook-Air.local>
2022-06-08 13:43:20 -0700

    Friendship add memcached

--- friendships/models.py
+++ friendships/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_delete
+from friendships.listeners import invalidate_following_cache
 
 
 class Friendship(models.Model):
@@ -28,3 +30,8 @@ class Friendship(models.Model):
 
     def __str__(self):
         return '{} followed {}'.format(self.from_user_id, self.to_user_id)
+
+
+# hookup listeners with invalidate cache key
+pre_delete.connect(invalidate_following_cache, sender=Friendship)
+post_save.connect(invalidate_following_cache, sender=Friendship)
