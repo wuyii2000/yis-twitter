46d4f548425249f660f8506a97a644932ab01735
Yi Wu <yiwu@Yis-MacBook-Air.local>
2022-06-27 18:16:49 -0700

    cache like_count and comment_count

--- likes/listeners.py
+++ likes/listeners.py
@@ -1,3 +1,6 @@
+from utils.redis_helper import RedisHelper
+
+
 def incr_likes_count(sender, instance, created, **kwargs):
     from tweets.models import Tweet
     from django.db.models import F
@@ -12,6 +15,7 @@ def incr_likes_count(sender, instance, created, **kwargs):
     # SQL Query:
     # UPDATE likes_count = likes_count +1 FROM tweets_table WHERE id=<instance.object_id>
     Tweet.objects.filter(id=instance.object_id).update(likes_count=F('likes_count') + 1)
+    RedisHelper.incr_count(instance.content_object, 'likes_count')
 
 
 def decr_likes_count(sender, instance, **kwargs):
@@ -23,3 +27,4 @@ def decr_likes_count(sender, instance, **kwargs):
         return
 
     Tweet.objects.filter(id=instance.object_id).update(likes_count=F('likes_count')-1)
+    RedisHelper.decr_count(instance.content_object, 'likes_count')
